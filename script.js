bb8f84ba9da93d511274aed79998be10bfd56ccd
powderrrrrr <116692891+Powder1378@users.noreply.github.com>
2026-01-15 14:02:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -562,7 +562,7 @@ function renderWheels(category) {
 
       <div class="wheel-info">
         <span class="wheel-type">${wheel.type}</span>
-        <span class="wheel-color ${wheel.chrome ? "no-color" : "color-ok"}">
+        <span class="wheel-color ${wheel.color ? "no-color" : "color-ok"}">
           ${wheel.chrome ? "Color変更可" : "Color変更不可"}
         </span>
       </div>
@@ -595,6 +595,7 @@ window.addEventListener("scroll", () => {
 scrollBtn.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
 
 
 
